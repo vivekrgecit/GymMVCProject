2e569bdaebcb16fa502a4870295477daa05d6941
vivekrgecit <62476309+vivekrgecit@users.noreply.github.com>
2020-05-21 12:37:27 +0530

    commint changes

--- GymMvcCore/GymMvcCore/Program.cs
+++ GymMvcCore/GymMvcCore/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -20,7 +21,11 @@ namespace GymMvcCore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>();
                 });
     }
 }
